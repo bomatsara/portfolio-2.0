4189e0f7d6031ea647f9176149e343bee4257470
KoliaKosyanenko <112539586+KoliaKosyanenko@users.noreply.github.com>
2024-05-31 15:05:49 +0300

    create mob-version

--- src/js/utils/const.js
+++ src/js/utils/const.js
@@ -1,3 +1,62 @@
-export const GLOBAL = {
-
-}
+export const PROJECTS_IMG = [
+  {
+    image1x: './img/my_projects/wallet-1x.jpeg',
+    image2x: './img/my_projects/wallet-2x.jpeg',
+    text: 'React, JavaScript, Node JS, Git',
+    description: 'Wallet webservice',
+  },
+  {
+    image1x: './img/my_projects/green_harvest-1x.jpeg',
+    image2x: './img/my_projects/green_harvest-2x.jpeg',
+    text: 'React, JavaScript, Node JS, Git',
+    description: 'Green harvest webservice',
+  },
+  {
+    image1x: './img/my_projects/english_excellence-1x.jpeg',
+    image2x: './img/my_projects/english_excellence-2x.jpeg',
+    text: 'React, JavaScript, Node JS, Git',
+    description: 'English Exellence website',
+  },
+  {
+    image1x: './img/my_projects/vyshyvanka-1x.jpeg',
+    image2x: './img/my_projects/vyshyvanka-2x.jpeg',
+    text: 'React, JavaScript, Node JS, Git',
+    description: 'Vyshyvanka vibes Landing Page',
+  },
+  {
+    image1x: './img/my_projects/powerpulse-1x.jpeg',
+    image2x: './img/my_projects/powerpulse-2x.jpeg',
+    text: 'React, JavaScript, Node JS, Git',
+    description: 'power pulse webservice ',
+  },
+  {
+    image1x: './img/my_projects/mimino-1x.jpeg',
+    image2x: './img/my_projects/mimino-2x.jpeg',
+    text: 'React, JavaScript, Node JS, Git',
+    description: 'mimino website',
+  },
+  {
+    image1x: './img/my_projects/chego-1x.jpeg',
+    image2x: './img/my_projects/chego-2x.jpeg',
+    text: 'React, JavaScript, Node JS, Git',
+    description: 'chego jewelry website',
+  },
+  {
+    image1x: './img/my_projects/energy_flow-1x.jpeg',
+    image2x: './img/my_projects/energy_flow-2x.jpeg',
+    text: 'React, JavaScript, Node JS, Git',
+    description: 'energy flow webservice ',
+  },
+  {
+    image1x: './img/my_projects/fruitbox-1x.jpeg',
+    image2x: './img/my_projects/fruitbox-2x.jpeg',
+    text: 'React, JavaScript, Node JS, Git',
+    description: 'fruitbox online store',
+  },
+  {
+    image1x: './img/my_projects/starlight-1x.jpeg',
+    image2x: './img/my_projects/starlight-2x.jpeg',
+    text: 'React, JavaScript, Node JS, Git',
+    description: 'starlight studio landing page',
+  },
+];
--- src/main.js
+++ src/main.js
@@ -4,3 +4,4 @@ import './js/block/faq/index.js'
 import './js/block/projects/index.js'
 import './js/block/reviews/index.js'
 import './js/block/work_together/index.js'
+import './js/utils/const.js';

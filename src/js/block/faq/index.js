9977fb8b673649cdb846ca985c3feb562047a6a4
Nazar Dovhanych <126353657+AndrzejSever@users.noreply.github.com>
2024-05-31 18:20:32 +0200

    mobile_version

---
+++ src/js/block/faq/index.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.ac_trigger').forEach(el => {
+  el.addEventListener('click', element => {
+
+    const allAccordions = document.querySelectorAll('.ac');
+
+    allAccordions.forEach(acc => {
+        if (acc !== element.currentTarget.closest('.ac')) {
+          acc.classList.remove('ac-click');
+        }
+      });
+
+    element.currentTarget.closest('.ac').classList.toggle('ac-click');
+  });
+});
+
+

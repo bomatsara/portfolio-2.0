3cd8cfe8e51a9ffc0aed19ed51406f77b4ce557a
KoliaKosyanenko <112539586+KoliaKosyanenko@users.noreply.github.com>
2024-06-03 16:59:01 +0300

    create header and JS logic

---
+++ src/js/block/header/index.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const mobMenuBtn = document.querySelector('.mob-menu-btn');
+  const mobCloseBtn = document.querySelector('.mob-close-btn');
+  const mobileMenu = document.querySelector('.mobile-menu');
+  const mobileLinks = document.querySelectorAll(
+    '.mobile-menu-item .header-menu-link'
+  );
+  const headerLinks = document.querySelectorAll('.header-menu-link');
+
+  mobMenuBtn.addEventListener('click', toggleMobileMenu);
+  mobCloseBtn.addEventListener('click', toggleMobileMenu);
+
+  mobileLinks.forEach(link => link.addEventListener('click', handleLinkClick));
+  headerLinks.forEach(link => link.addEventListener('click', handleLinkClick));
+
+  function toggleMobileMenu() {
+    mobileMenu.classList.toggle('open');
+  }
+
+  function handleLinkClick(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      scrollToTarget(targetElement);
+      if (mobileMenu.classList.contains('open')) {
+        toggleMobileMenu();
+      }
+    }
+  }
+
+  function scrollToTarget(targetElement) {
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: 'smooth',
+    });
+  }
+});

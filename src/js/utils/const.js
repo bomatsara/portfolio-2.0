d957536b404d87cf124c1a917464b12bda906a23
Andrii Bomatsara <andrii.bomatsara@vista.com>
2024-06-04 13:29:17 +0300

    Minor fix

--- src/js/utils/const.js
+++ src/js/utils/const.js
@@ -1,70 +1,91 @@
+import wallet1xImg from '../../img/my_projects/wallet-1x.png';
+import wallet2xImg from '../../img/my_projects/wallet-2x.png';
+import greenHarvest1xImg from '../../img/my_projects/green_harvest-1x.png';
+import greenHarvest2xImg from '../../img/my_projects/green_harvest-2x.png';
+import englishExcellence1xImg from '../../img/my_projects/english_excellence-1x.png';
+import englishExcellence2xImg from '../../img/my_projects/english_excellence-2x.png';
+import vyshyvanka1xImg from '../../img/my_projects/vyshyvanka-1x.png';
+import vyshyvanka2xImg from '../../img/my_projects/vyshyvanka-2x.png';
+import powerPulse1xImg from '../../img/my_projects/powerpulse-1x.png';
+import powerPulse2xImg from '../../img/my_projects/powerpulse-2x.png';
+import mimino1xImg from '../../img/my_projects/mimino-1x.png';
+import mimino2xImg from '../../img/my_projects/mimino-2x.png';
+import chego1xImg from '../../img/my_projects/chego-1x.png';
+import chego2xImg from '../../img/my_projects/chego-2x.png';
+import energyFlow1xImg from '../../img/my_projects/energy_flow-1x.png';
+import energyFlow2xImg from '../../img/my_projects/energy_flow-2x.png';
+import fruitbox1xImg from '../../img/my_projects/fruitbox-1x.png';
+import fruitbox2xImg from '../../img/my_projects/fruitbox-2x.png';
+import starlight1xImg from '../../img/my_projects/starlight-1x.png';
+import starlight2xImg from '../../img/my_projects/starlight-2x.png';
+
 export const PROJECTS_IMG = [
   {
-    image1x: '/img/my_projects/wallet-1x.png',
-    image2x: '/img/my_projects/wallet-2x.png',
+    image1x: wallet1xImg,
+    image2x: wallet2xImg,
     text: 'React, JavaScript, Node JS, Git',
     description: 'Wallet webservice',
     alt: 'Wallet webservice',
   },
   {
-    image1x: '/img/my_projects/green_harvest-1x.png',
-    image2x: '/img/my_projects/green_harvest-2x.png',
+    image1x: greenHarvest1xImg,
+    image2x: greenHarvest2xImg,
     text: 'React, JavaScript, Node JS, Git',
     description: 'Green harvest webservice',
     alt: 'Green harvest webservice',
   },
   {
-    image1x: '/img/my_projects/english_excellence-1x.png',
-    image2x: '/img/my_projects/english_excellence-2x.png',
+    image1x: englishExcellence1xImg,
+    image2x: englishExcellence2xImg,
     text: 'React, JavaScript, Node JS, Git',
-    description: 'English Exellence website',
-    alt: 'English Exellence website',
+    description: 'English Excellence website',
+    alt: 'English Excellence website',
   },
   {
-    image1x: '/img/my_projects/vyshyvanka-1x.png',
-    image2x: '/img/my_projects/vyshyvanka-2x.png',
+    image1x: vyshyvanka1xImg,
+    image2x: vyshyvanka2xImg,
     text: 'React, JavaScript, Node JS, Git',
     description: 'Vyshyvanka vibes Landing Page',
     alt: 'Vyshyvanka vibes Landing Page',
   },
   {
-    image1x: '/img/my_projects/powerpulse-1x.png',
-    image2x: '/img/my_projects/powerpulse-2x.png',
+    image1x: powerPulse1xImg,
+    image2x: powerPulse2xImg,
     text: 'React, JavaScript, Node JS, Git',
     description: 'power pulse webservice',
     alt: 'power pulse webservice',
   },
   {
-    image1x: '/img/my_projects/mimino-1x.png',
-    image2x: '/img/my_projects/mimino-2x.png',
+    image1x: mimino1xImg,
+    image2x: mimino2xImg,
     text: 'React, JavaScript, Node JS, Git',
     description: 'mimino website',
     alt: 'mimino website',
   },
   {
-    image1x: '/img/my_projects/chego-1x.png',
-    image2x: '/img/my_projects/chego-2x.png',
+    image1x: chego1xImg,
+    image2x: chego2xImg,
     text: 'React, JavaScript, Node JS, Git',
     description: 'chego jewelry website',
     alt: 'chego jewelry website',
   },
   {
-    image1x: '/img/my_projects/energy_flow-1x.png',
-    image2x: '/img/my_projects/energy_flow-2x.png',
+    image1x: energyFlow1xImg,
+    image2x: energyFlow2xImg,
     text: 'React, JavaScript, Node JS, Git',
     description: 'energy flow webservice',
     alt: 'energy flow webservice',
   },
   {
-    image1x: '/img/my_projects/fruitbox-1x.png',
-    image2x: '/img/my_projects/fruitbox-2x.png',
+    image1x: fruitbox1xImg,
+    image2x: fruitbox2xImg,
     text: 'React, JavaScript, Node JS, Git',
     description: 'fruitbox online store',
     alt: 'fruitbox online store',
   },
   {
-    image1x: '/img/my_projects/starlight-1x.png',
-    image2x: '/img/my_projects/starlight-2x.png',
+    image1x: starlight1xImg,
+    image2x: starlight2xImg,
     text: 'React, JavaScript, Node JS, Git',
     description: 'starlight studio landing page',
     alt: 'starlight studio landing page',
